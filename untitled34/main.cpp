927ffd44e637ca3f5f729cec6f58a2030b7cea62
vasya5500u <klevach.vasyl@gmail.com>
2024-05-12 20:16:32 +0300

    ex4 thread(B)

--- untitled34/main.cpp
+++ untitled34/main.cpp
@@ -13,3 +13,15 @@ void threadA() {
     mutex2.unlock();
     mutex1.unlock();
 }
+
+void threadB() {
+    mutex2.lock();
+    std::cout << "Thread B acquired mutex2" << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    mutex1.lock();
+    std::cout << "Thread B acquired mutex1" << std::endl;
+    mutex1.unlock();
+    mutex2.unlock();
+}
+
+

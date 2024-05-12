dc3e312cc9e038b69ce7660e97ecbc2d2826380b
vasya5500u <klevach.vasyl@gmail.com>
2024-05-12 20:16:04 +0300

    ex4 thread(A)

--- untitled34/main.cpp
+++ untitled34/main.cpp
@@ -1,29 +1,15 @@
 #include <iostream>
 #include <thread>
-#include <vector>
 #include <mutex>
 
-int counter = 0;
-std::mutex counter_mutex;
+std::mutex mutex1, mutex2;
 
-int main() {
-    std::vector<std::thread> threads;
-
-    for (int i = 1; i <= 3; ++i) {
-        threads.emplace_back([i]() {
-            int squared = i * i;
-            std::cout << "Thread " << i << ": " << squared << std::endl;
-
-            counter_mutex.lock();
-            counter += i;
-            std::cout << "Counter: " << counter << std::endl;
-            counter_mutex.unlock();
-        });
-    }
-
-    for (auto& thread : threads) {
-        thread.join();
-    }
-
-    return 0;
+void threadA() {
+    mutex1.lock();
+    std::cout << "Thread A acquired mutex1" << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    mutex2.lock();
+    std::cout << "Thread A acquired mutex2" << std::endl;
+    mutex2.unlock();
+    mutex1.unlock();
 }
